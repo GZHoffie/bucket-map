87f408766882db942e9b0202e9c0d9484025d10a
Gu Zhenhao <zhenhao.gu@u.nus.edu>
2022-07-10 21:09:13 +0800

    Add kmer frequency calculation algorithm

---
+++ mcomp_project/algorithms/kmer_frequency.py
@@ -0,0 +1,170 @@
+from Bio import SeqIO
+from mcomp_project.utils import *
+import numpy as np
+import pickle
+
+class KMerFrequency:
+    def __init__(self, order=7, region_length=100000, read_length=100, substitution_rate=0.02, prior=0.001) -> None:
+        self.order = order
+        self.region_length = region_length
+        self.read_length = read_length
+        self.substitution_rate = substitution_rate
+        self.prior = prior
+        self.M = None
+    
+
+    def _new_frequency_list(self):
+        """
+        Initialize a new vector and fill everything with `self.prior`
+        """
+        frequency_list = np.zeros([4] * self.order)
+        frequency_list.fill(self.prior)
+        return frequency_list
+    
+
+    def _insert_into_frequency_list(self, sequence, frequency_list, allow_substitution=True):
+        """
+        Count the sequence into the markov chain. In particular, we want
+        frequency_list[sequence] += 1. If we allow substitution, we also do
+        frequency_list[variant_of_sequence] += self.substitution_rate.
+
+        The variant of a sequence is basically all the sequences that can be 
+        obtained by substituting a character in the original sequence.
+
+        Args:
+            sequence: A sequence of length `self.order`
+            frequency_list: A matrix storing markov chain parameters
+            allow_substitution: whether we allow substitution.
+        """
+        try:
+            index = [char_to_index_map[c] for c in sequence]
+        except:
+            return
+        if allow_substitution:
+            for i in range(self.order):
+                index_i = index[i]
+                index[i] = ...
+                frequency_list[tuple(index)] += self.substitution_rate
+                index[i] = index_i
+            
+            frequency_list[tuple(index)] += 1 - self.order * self.substitution_rate
+        else:
+            frequency_list[tuple(index)] += 1
+
+
+        
+    
+    def _sequence_to_index(self, sequence):
+        """
+        Turn a `self.order`-mer into an index between 0 and
+        4 ** `self.order` - 1. Used to store the frequency of the k-mer.
+
+        Args:
+            sequence: A sequence of length `self.order`
+        
+        Returns:
+            The index of the sequence or None if illegal characters appear
+            in the sequence.
+        """
+        assert len(sequence) == self.order, f"The length of input sequence {sequence} should be of length {self.order}"
+        try:
+            index = [char_to_index_map[c] for c in sequence]
+        except:
+            return None
+        return np.ravel_multi_index(index, [4] * self.order)
+    
+
+    def _store_frequency_list(self, frequency_list):
+        """
+        Store the current markov chain parameters in self.M. We do a
+        normalization and store the log of each parameters.
+
+        Args:
+            frequency_list: an array of markov chain parameters
+        """
+        frequency_list /= np.sum(frequency_list)
+        frequency_list = np.log(frequency_list)
+        self.M = frequency_list.flatten() if self.M is None else np.vstack([self.M, frequency_list.flatten()])
+
+            
+
+    def read(self, fasta_file_name, output_file=True):
+        """
+        Read the fasta file and store the Markov Chain frequencies in
+        self.M, and print the related information.
+
+        Args:
+            fasta_file_name: the string consisting of the path to the
+                fasta file.
+        """
+        print("Opening FASTA file:", fasta_file_name)
+        fasta_sequences = SeqIO.parse(open(fasta_file_name), 'fasta')
+        for fasta in fasta_sequences:
+            sequence_length = len(str(fasta.seq))
+            print("... processing sequence", fasta.id, "with length", sequence_length)
+            print("Estimated space usage: ", sequence_length / self.region_length * (4 ** self.order) * 4 / (1024 ** 2), "MB")
+            
+            # start counting the frequency the k-mers
+            index = 0
+            frequency_list = self._new_frequency_list()
+            while index + self.order < sequence_length:
+
+                current_k_mer = fasta.seq[index:index + self.order]
+                self._insert_into_frequency_list(current_k_mer, frequency_list)
+                #print(current_k_mer, self._sequence_to_index(current_k_mer))
+                index += 1
+                if index % self.region_length == 0:
+                    # Complete this region, store the markov chain parameters
+                    self._store_frequency_list(frequency_list)
+                    
+                    # Create new Markov chain
+                    frequency_list = self._new_frequency_list()
+            
+            # Complete this region, store the markov chain parameters
+            self._store_frequency_list(frequency_list)
+                    
+        
+        #print(self.M)
+        if output_file:
+            with open(f"{fasta_file_name}_frequency_list.pickle", "wb") as f:
+                pickle.dump(self.M, f, pickle.HIGHEST_PROTOCOL)
+    
+    def store(self, pickle_file_name):
+        """
+        Store the self.M matrix into a pickle file.
+        """
+        with open(pickle_file_name, "wb") as f:
+            pickle.dump(self.M, f)
+    
+    def load(self, pickle_file_name):
+        """
+        load the pickle file into self.M.
+        """
+        with open(pickle_file_name, 'rb') as f:
+            self.M = pickle.load(f)
+    
+    def query(self, sequence):
+        k_mers = np.zeros((4 ** self.order, 1))
+        for i in range(len(sequence) - self.order):
+            current_k_mer = sequence[i:i + self.order]
+            index = self._sequence_to_index(current_k_mer)
+            if index is not None:
+                k_mers[index] += 1
+        
+        log_probability = np.dot(self.M, k_mers)
+        #print(log_probability)
+        return np.argmax(log_probability)
+
+        
+
+            
+        
+
+
+
+if __name__ == "__main__":
+    mc = KMerFrequency(order=7, region_length=20000)
+    mc._insert_into_frequency_list("ACNCN", mc._new_frequency_list())
+    #mc.read("/home/zhenhao/data/SRR611076/sequence.fasta")
+    #print(mc.query("GCTCTTTCCCCGGAAACCATTGAAATCGGACGGTTTAGTGAAAATGGAGGATCAAGTTGGGTTTGGGTTC"))
+        
--- mcomp_project/test/test_kmer_frequency.py
+++ mcomp_project/test/test_kmer_frequency.py
@@ -1,13 +1,13 @@
-from mcomp_project.algorithms.markov_chain import DNAMarkovChain
+from mcomp_project.algorithms.kmer_frequency import KMerFrequency
 from Bio import SeqIO
 import numpy as np
 import time
 
-def test_markov_chain(fasta_file_name, pickle_file_name=None, test_num=10000, order=6, region_length=10000, read_length=100, substitution_rate=0.02, prior=0.001):
-    mc = DNAMarkovChain(order, region_length, read_length, substitution_rate, prior)
+def test_kmer_frequency(fasta_file_name, pickle_file_name=None, test_num=1000, order=7, region_length=10000, read_length=100, substitution_rate=0.02, prior=0.001):
+    mc = KMerFrequency(order, region_length, read_length, substitution_rate, prior)
     start_time = time.time()
     if pickle_file_name is None:
-        mc.read(fasta_file_name)
+        mc.read(fasta_file_name, output_file=False)
     else:
         mc.load(pickle_file_name)
     print("Reading time:", time.time() - start_time, "s")
@@ -19,14 +19,13 @@ def test_markov_chain(fasta_file_name, pickle_file_name=None, test_num=10000, or
         for _ in range(test_num):
             index = np.random.randint(0, sequence_length - read_length - 1)
             region = index // region_length
-            #print(fasta.seq[index:index + read_length])
             predict_region = mc.query(fasta.seq[index:index + read_length])
             correct += (region == predict_region)
-            #print(region, predict_region)
+            print(region, predict_region)
     
         print("Query time:", time.time() - start_time, "s")
     
     print("Correct rate:", correct / test_num)
 
 if __name__ == "__main__":
-    test_markov_chain("/home/zhenhao/mcomp-dissertation/sequence_sample.fasta", "/home/zhenhao/mcomp-dissertation/sequence_sample.fasta_markov_chain.pickle")
+    test_kmer_frequency("/home/zhenhao/mcomp-dissertation/sequence_sample.fasta")#, "/home/zhenhao/mcomp-dissertation/sequence_sample.fasta_markov_chain.pickle")
