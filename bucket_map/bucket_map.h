6446c7495860b169f9852611c4f7eefa2a6edc92
Gu Zhenhao <zhenhao.gu@u.nus.edu>
2022-08-31 00:52:34 +0800

    Add bucket map indexing functions

--- bucket_map/bucket_map.h
+++ bucket_map/bucket_map.h
@@ -3,52 +3,105 @@
 
 #include <string>
 #include <vector>
+#include <cmath>
 
+#include <seqan3/alignment/configuration/all.hpp>
+#include <seqan3/alignment/pairwise/align_pairwise.hpp>
+#include <seqan3/argument_parser/all.hpp>
+#include <seqan3/io/sam_file/output.hpp>
+#include <seqan3/io/sequence_file/input.hpp>
+#include <seqan3/search/all.hpp>
 #include <seqan3/search/fm_index/bi_fm_index.hpp>
+ 
+#include <cereal/archives/binary.hpp>
 
-/**
- * @brief A class storing the information of a bucket, namely the 
- * sequence that the bucket belongs to, as well as the index of bucket.
- */
-class bucket_info {
-    std::string sequence_id;
-    unsigned int bucket_id;
-};
 
 
 class bucket_mapper {
 private:
-    std::string index_id;                          // a string indicating the handle to index files
-    std::vector<bucket_info> buckets;              // vector storing all bucket info
-    std::vector<seqan3::bi_fm_index> bucket_index; // index files for each buckets
-    unsigned int bucket_length;                    // maximum length of each bucket
+    std::string index_id;                                // a string indicating the handle to index files
+    std::vector<std::string> bucket_id;                  // vector storing all bucket info
+    std::vector<std::vector<seqan3::dna5>> bucket_seq;   // sequence for each bucket
+    unsigned int bucket_length;                          // maximum length of each bucket
+    unsigned int read_length;                            // maximum length of each short read
 
-public:
-    bucket_mapper(int length) {
-        bucket_length = length;
+    void _create_index(std::filesystem::path const & index_directory) {
+        /**
+         * @brief Create index files for the buckets. 
+         * @remark needs to be run after we fill `bucket_id` and `bucket_seq`.
+         */
+        for (int i = 0; i < bucket_id.size(); i++) {
+            seqan3::bi_fm_index index{bucket_seq[i]};
+            // store the index in a file
+            {
+                std::ofstream os{index_directory / (std::to_string(i) + ".index"), std::ios::binary};
+                cereal::BinaryOutputArchive oarchive{os};
+                oarchive(index);
+            }
+        
+        }
     }
 
-    int index(std::string fasta_file_name, bool output_index_file) {
+public:
+    bucket_mapper(std::string id, unsigned int bucket_len, unsigned int read_len) {
+        index_id = id;
+        bucket_length = bucket_len;
+        read_length = read_len;
+    }
+
+    unsigned int index(std::filesystem::path const & fasta_file_name, std::filesystem::path const & index_directory) {
         /**
-         * @brief Read the fasta file, index each bucket and store in
-         * `bucket_index`.
+         * @brief Read the fasta file, index each bucket.
          * @param fasta_file_name the name of the file containing reference genome.
-         * @param output_index_file whether we output the index file using cereal.
+         * @param index_directory the name of the directory to store the index. It has to be
+         *                        either empty directory or not created yet.
          * @returns the total number of buckets.
          */
+        // Create directory if directory is not created yet.
+        if (!std::filesystem::create_directories(index_directory)) {
+            seqan3::debug_stream << "[WARNING]\t" << "The specified index directory "
+                                 << index_directory << " is already created." << '\n';
+            // count the number 
+            unsigned int count = 0;
+            for (const auto& entry : std::filesystem::directory_iterator(index_directory)) {
+                if (entry.path().extension() == ".index")
+                    ++count;
+            }
+            if (count > 0) {
+                seqan3::debug_stream << "[INFO]\t\t" << "The number of index files in directory: " 
+                                     << count << "." << '\n';
+                return count;
+            }
+        }
         seqan3::sequence_file_input reference_genome{fasta_file_name};
         std::string previous_id = "";
         int index = 0;
         for (auto && record : reference_genome) {
-            if (previous_id != record.id()) {
-                previous_id = record.id();
-                index = 0;
+            // Divide the record into buckets
+            float total_length = (float) record.sequence().size();
+            int num_buckets = (int) ceil(total_length / bucket_length);
+            std::cout << record.id() << " with length " << total_length
+                      << " divided into " << num_buckets << " buckets." << std::endl;
+            
+            // read each buckets
+            for (int i = 0; i < num_buckets; i++) {
+                bucket_id.push_back(record.id() + " | " + std::to_string(i));
+                int start = i * bucket_length;
+                int end = start + bucket_length + read_length;
+                if (end > record.sequence().size()) {
+                    end = record.sequence().size();
+                }
+                std::vector<seqan3::dna5> bucket_sequence(&record.sequence()[start], &record.sequence()[end]);
+                bucket_seq.push_back(bucket_sequence);
             }
-            buckets.push_back(new bucket_info(record.id(), index));
-            index++;
-        }
-    }
 
+            // create index and store in the index_directory
+            _create_index(index_directory);
+        }
+        seqan3::debug_stream << "[INFO]\t\t" << "The number of index files created: " 
+                             << bucket_id.size() << "." << '\n';
+        return bucket_id.size();
+    }
 };
 
 
--- bucket_map/main.cpp
+++ bucket_map/main.cpp
@@ -1,7 +1,14 @@
 #include <seqan3/core/debug_stream.hpp>
- 
+#include "bucket_map.h"
+
+
 int main()
 {
+    bucket_mapper * mapper = new bucket_mapper("test", 10000, 100);
+    mapper->index("/home/zhenhao/mcomp-dissertation/sequence_sample.fasta", "test");
+
+
+
     seqan3::debug_stream << "Hello World!\n";
     return 0;
 }
