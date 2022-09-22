02bb06f74f4a4a698ead37d5ea94a90f239f1266
Gu Zhenhao <zhenhao.gu@u.nus.edu>
2022-09-23 02:42:23 +0800

    Allow adding error to simulated short reads

--- bucket_map/main.cpp
+++ bucket_map/main.cpp
@@ -1,69 +1,14 @@
 #include "mapper/q_gram_map.h"
-
-#include <seqan3/search/kmer_index/shape.hpp>
-
-#include <cstdlib>
-#include <ctime>
-
-using seqan3::operator""_shape;
-
-class short_read_simulator {
-private:
-    std::vector<std::vector<seqan3::dna4>> bucket_sequence;
-    int bucket_length, read_length;
-
-    // TODO: Add error in the short read
-
-
-public:
-    short_read_simulator(int bucket_len, int read_len) {
-        bucket_length = bucket_len;
-        read_length = read_len;
-    }
-
-    void read(std::filesystem::path const & fasta_file_name) {
-        seqan3::sequence_file_input reference_genome{fasta_file_name};
-        for (auto && record : reference_genome) {
-            // Divide the record into buckets
-            float total_length = (float) record.sequence().size();
-            int num_buckets = (int) ceil(total_length / bucket_length);
-            
-            // read each bucket
-            for (int i = 0; i < num_buckets; i++) {
-                int start = i * bucket_length;
-                int end = start + bucket_length + read_length;
-                if (end > record.sequence().size()) {
-                    end = record.sequence().size();
-                }
-                std::vector<seqan3::dna4> seq(&record.sequence()[start], &record.sequence()[end]);
-                bucket_sequence.push_back(seq);
-            }
-        }
-    }
-
-    std::pair<std::vector<seqan3::dna4>, int> sample() {
-        int bucket = rand() % bucket_sequence.size();
-        std::vector<seqan3::dna4> current_bucket = bucket_sequence[bucket];
-        int start = rand() % bucket_length;
-        int end = start + read_length;
-        if (end > current_bucket.size()) {
-            end = current_bucket.size();
-        }
-        
-        std::vector<seqan3::dna4> sample_sequence(current_bucket.begin() + start, current_bucket.begin() + end);
-        return std::make_pair(sample_sequence, bucket);
-    }
-};
+#include "simulation/short_read_simulator.h"
 
 
 int main() {
-    srand(time(NULL));
 
-    q_gram_mapper<700> map(10000, 100, 0b11101001010011_shape, 10, 2);
+    q_gram_mapper<700> map(10000, 100, 0b11101001010011_shape, 20, 10);
     map.read("/home/zhenhao/mcomp-dissertation/sequence_sample.fasta");
     map.store("/home/zhenhao/mcomp-dissertation/build/sequence_sample");
 
-    short_read_simulator sim(10000, 100);
+    short_read_simulator sim(10000, 100, 0.002, 0.00025, 0.00025);
     sim.read("/home/zhenhao/mcomp-dissertation/sequence_sample.fasta");
     int correct = 0;
     int total_size = 0;
---
+++ bucket_map/simulation/short_read_simulator.h
@@ -0,0 +1,136 @@
+#include <seqan3/search/kmer_index/shape.hpp>
+#include <seqan3/alphabet/all.hpp>
+
+#include <cstdlib>
+#include <ctime>
+#include <random>
+
+using seqan3::operator""_dna4;
+
+class short_read_simulator {
+    /**
+     * @brief A tool to randomly generate short reads based on a reference genome.
+     *        It allows setting of substitution/indel error rates and random generation
+     *        of errors.
+     */
+private:
+    // variables storing the sequence information
+    std::vector<std::vector<seqan3::dna4>> bucket_sequence;
+    int bucket_length, read_length;
+
+    // Error generation
+    std::vector<seqan3::dna4> neucleotides{ 'A'_dna4, 'C'_dna4, 'G'_dna4, 'T'_dna4 };
+    std::poisson_distribution<int>* substitution_dist; 
+    std::poisson_distribution<int>* insertion_dist;
+    std::poisson_distribution<int>* deletion_dist;
+
+    // Random number generator
+    std::mt19937* gen;
+
+    // Error generating functions
+    void _add_substitution(std::vector<seqan3::dna4>& sequence) {
+        sequence[rand() % sequence.size()] = neucleotides[rand() % 4];
+    }
+
+    void _add_insertion(std::vector<seqan3::dna4>& sequence) {
+        sequence.insert(sequence.begin() + rand() % sequence.size(), neucleotides[rand() % 4]);
+    }
+
+    void _add_deletion(std::vector<seqan3::dna4>& sequence) {
+        sequence.erase(sequence.begin() + rand() % sequence.size());
+    }
+
+public:
+    short_read_simulator(int bucket_len, int read_len, float substitution_rate = 0, 
+                         float insertion_rate = 0, float deletion_rate = 0) {
+        // set a random seed
+        srand(time(NULL));
+        
+        // Set indexing-related numbers
+        bucket_length = bucket_len;
+        read_length = read_len;
+        
+        // Set the error rates
+        substitution_dist = new std::poisson_distribution<int>(substitution_rate);
+        insertion_dist = new std::poisson_distribution<int>(insertion_rate);
+        deletion_dist = new std::poisson_distribution<int>(deletion_rate);
+    }
+
+    ~short_read_simulator() {
+        delete substitution_dist, insertion_dist, deletion_dist;
+    }
+
+    void add_errors(std::vector<seqan3::dna4>& sequence, int substitutions, int deletions, int insertions) {
+        /**
+         * @brief Add errors to a short read sequence.
+         * @param sequence the short read sequence to be modified.
+         * @param substitutions number of substitutions to make.
+         * @param deletions number of deletions to make.
+         * @param insertions number of insertions to make.
+         */
+        for (int i = 0; i < deletions; i++) _add_deletion(sequence);
+        for (int i = 0; i < insertions; i++) _add_insertion(sequence);
+        for (int i = 0; i < substitutions; i++) _add_substitution(sequence);
+    }
+
+    void simulate_errors(std::vector<seqan3::dna4>& sequence) {
+        /**
+         * @brief Do simulation and generate the number of substitution, deletion and insertion randomly
+         *        from poisson distributions with lamda = substitution_rate, deletion_rate and 
+         *        insertion_rate, respectively.
+         * @note the number of errors generated might always be 0 if error rates are too small. Might consider
+         *       setting them higher than actual error rate.
+         */
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        add_errors(sequence,
+                   (*substitution_dist)(gen),
+                   (*deletion_dist)(gen),
+                   (*insertion_dist)(gen));
+    }
+
+    void read(std::filesystem::path const & fasta_file_name) {
+        /**
+         * @brief Read the fasta file and store the sequences for each bucket. The reference
+         *        genome is then used for short read generation.
+         * @param fasta_file_name the name of the sequence file to be read.
+         */
+        seqan3::sequence_file_input reference_genome{fasta_file_name};
+        for (auto && record : reference_genome) {
+            // Divide the record into buckets
+            float total_length = (float) record.sequence().size();
+            int num_buckets = (int) ceil(total_length / bucket_length);
+            
+            // read each bucket
+            for (int i = 0; i < num_buckets; i++) {
+                int start = i * bucket_length;
+                int end = start + bucket_length + read_length;
+                if (end > record.sequence().size()) {
+                    end = record.sequence().size();
+                }
+                std::vector<seqan3::dna4> seq(&record.sequence()[start], &record.sequence()[end]);
+                bucket_sequence.push_back(seq);
+            }
+        }
+    }
+
+    std::pair<std::vector<seqan3::dna4>, int> sample(bool simulate_error = true) {
+        /**
+         * @brief Take a sample short read from the reference genome and add errors to it.
+         * @param simulate_error whether we add random error to the sequence or not.
+         * @return a pair storing <the generated short read, the bucket id it actually belongs to>.
+         */
+        int bucket = rand() % bucket_sequence.size();
+        std::vector<seqan3::dna4> current_bucket = bucket_sequence[bucket];
+        int start = rand() % (current_bucket.size() - read_length);
+        int end = start + read_length;
+        if (end > current_bucket.size()) {
+            end = current_bucket.size();
+        }
+        std::vector<seqan3::dna4> sample_sequence(current_bucket.begin() + start, current_bucket.begin() + end);
+        if (simulate_error) {
+            simulate_errors(sample_sequence);
+        }
+        return std::make_pair(sample_sequence, bucket);
+    }
+};
