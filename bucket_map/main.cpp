812e45f0de7e957bfdd033cabec560af2021bf89
Gu Zhenhao <guzh@nus.edu.sg>
2022-09-29 17:13:52 +0800

    Adjust read length

--- bucket_map/main.cpp
+++ bucket_map/main.cpp
@@ -6,11 +6,14 @@
 int main() {
     std::filesystem::path genome_file = "/mnt/c/data/Egu.v3.genome_f.fasta";
 
-    //q_gram_mapper<34570> map(50000, 100, 0b1110100101001101_shape, 20, 10);
+    int bucket_length = 50000;
+    int read_length = 150;
+
+    //q_gram_mapper<34570> map(bucket_length, read_length, 0b1110100101001101_shape, 20, 10);
     //map.read(genome_file);
     //map.store("/home/zhenhao/mcomp-dissertation/build/sequence_sample");
 
-    short_read_simulator sim(50000, 100, 0.002, 0.00025, 0.00025);
+    short_read_simulator sim(bucket_length, read_length, 0.002, 0.00025, 0.00025);
     sim.read(genome_file);
     sim.generate_fastq_file("/mnt/c/data/test", "sim", 1000000);
     /*
